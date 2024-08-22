bd9f23ff102f9699df659332bad687140ed36196
Juan José <juanjoos1905@gmail.com>
2024-08-22 11:04:29 -0500

    Create method add_tag

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,7 +1,7 @@
 class Todo:
-    def __init__(self, code_id: int, tittle: str, description: str):
+    def __init__(self, code_id: int, title: str, description: str):
         self.code_id: int = code_id
-        self.title: str = tittle
+        self.title: str = title
         self.description: str = description
         self.completed: bool = False
         self.tags: list[str] = []
@@ -14,7 +14,7 @@ class Todo:
         for i in self.tags:
             if tag == self.tags:
                 tag_count += 1
-        if tag_count != 0:
+        if tag_count == 0:
             self.tags.append(tag)
 
 
